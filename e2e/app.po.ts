69292ad1fdb4b5266559682af646be714e692679
rupertlion <rupertlion@gmail.com>
2018-10-13 11:42:42 +0200

    Finding title element calculator for e2e test - passing

--- e2e/app.po.ts
+++ e2e/app.po.ts
@@ -2,15 +2,15 @@ import { browser, by, element } from 'protractor';
 
 export class Page {
 
-  navigateTo(destination) {
-    return browser.get(destination);
-  }
+    navigateTo(destination) {
+        return browser.get(destination);
+    }
 
-  getTitle() {
-    return browser.getTitle();
-  }
+    getTitle() {
+        return browser.getTitle();
+    }
 
- getPageOneTitleText() {
-    return element(by.tagName('page-one')).element(by.tagName('ion-title')).getText();
-  }
+    getPageOneTitleText() {
+        return element(by.tagName('ion-title')).getText();
+    }
 }
