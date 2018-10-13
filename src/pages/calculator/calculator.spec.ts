c1546bdd0a7746c165ad8bfa874163993e78556e
rupertlion <rupertlion@gmail.com>
2018-10-13 15:21:57 +0200

    Updating tests for calculator to inject

--- src/pages/calculator/calculator.spec.ts
+++ src/pages/calculator/calculator.spec.ts
@@ -6,24 +6,23 @@ import { SplashScreen } from "@ionic-native/splash-screen";
 import { PlatformMock, StatusBarMock, SplashScreenMock, NavControllerMock, NavParamsMock } from "ionic-mocks";
 
 describe("CalculatorPage", () => {
-  let calculatorpage;
-  let fixture;
+    let calculatorpage;
+    let fixture;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [
-        CalculatorPage
-      ],
-      imports: [IonicModule.forRoot(CalculatorPage)],
-      providers: [
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+        declarations: [
+            CalculatorPage
+        ],
+        imports: [IonicModule.forRoot(CalculatorPage)],
+        providers: [
         { provide: Platform, useFactory: () => PlatformMock.instance() },
         { provide: StatusBar, useFactory: () => StatusBarMock.instance() },
         { provide: SplashScreen, useFactory: () => SplashScreenMock.instance() },
         { provide: NavController, useFactory: () => NavControllerMock.instance() },
         { provide: NavParams, useFactory: () => NavParamsMock.instance() },
-        // { provide: CalculatorPage, useFactory: () => CalculatorPageMock.instance() }
-    ]
-    }).compileComponents();
+        ]
+        }).compileComponents();
     }));
 
     beforeEach(() => {
@@ -44,17 +43,19 @@ describe("CalculatorPage", () => {
         expect(calculatorpage.calculateBMI).toHaveBeenCalled();
     });
 
-    it("calculate function should call calculator calculateBMI function", () => {
-        
-        calculatorpage.weight = 80
-        calculatorpage.height = 180
-        spyOn(calculatorpage, "calculateBMI").and.returnValue("Normal");
+    it("calculate function should call calculator calculateBMI function", () => inject(
+        [CalculatorPage],
+        inputs => {        
+            calculatorpage.weight = 80
+            calculatorpage.height = 180
+            spyOn(inputs, "calculateBMI").and.returnValue("Normal");
 
-        calculatorpage.calculateBMI();
+            calculatorpage.calculateBMI();
 
-        expect(calculatorpage.calculateBMI).toHaveBeenCalled();
-        expect(calculatorpage.height).toEqual(180);
-        expect(calculatorpage.weight).toEqual(80);
-        expect(calculatorpage.calculateBMI).toHaveBeenCalledWith()
-        });
+            expect(calculatorpage.calculateBMI).toHaveBeenCalled();
+            expect(calculatorpage.height).toEqual(180);
+            expect(calculatorpage.weight).toEqual(80);
+            expect(calculatorpage.calculateBMI).toHaveBeenCalledWith(24.69)
+        }
+    ));
 });
