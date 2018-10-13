a1b7a126df678fbaea1b9c9e2b9b1fd2ad1b2b74
rupertlion <rupertlion@gmail.com>
2018-10-13 19:06:23 +0200

    Removing additional elements of happymessage and sorting feature tests

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,6 @@ import { MyApp } from './app.component';
 import { AboutPage } from '../pages/about/about';
 import { TabsPage } from '../pages/tabs/tabs';
 import { CalculatorPage } from '../pages/calculator/calculator';
-import { HappymessagePage } from '../pages/happymessage/happymessage';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
@@ -16,8 +15,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     MyApp,
     AboutPage,
     TabsPage,
-    CalculatorPage,
-    HappymessagePage
+    CalculatorPage
   ],
   imports: [
     BrowserModule,
@@ -28,8 +26,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     MyApp,
     AboutPage,
     TabsPage,
-    CalculatorPage,
-    HappymessagePage
+    CalculatorPage
   ],
   providers: [
     StatusBar,
